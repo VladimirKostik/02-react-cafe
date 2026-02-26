7d1d46146ffbd258e31b54942189fe7db633f5a4
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-02-27 00:01:44 +0200

    fix: remove dot from CafeInfo description

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -3,9 +3,9 @@ const CafeInfo = () => {
   return (
     <div className={css.container}>
   <h1 className={css.title}>Sip Happens Café</h1>
-  <p className={css.description}>
-    Please rate our service by selecting one of the options below
-  </p>
+ <p className={css.description}>
+  Please rate our service by selecting one of the options below
+</p>
 </div>
   );
 };
